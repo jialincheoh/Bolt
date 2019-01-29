481e7133a3f6a1a1a8108f8908a505353c912e51
exponentialbit1024 <shah255@purdue.edu>
2019-01-28 20:24:36 -0500

    blueprint setup + dbCon object for connector + config file

---
+++ backend/app/accountBp.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, session, jsonify, request, render_template,    session, redirect
+from flask_cors import CORS
+import requests
+import json
+
+from .config import *
+
+accountRoutes = Blueprint('accountRoutes', __name__)
+CORS(accountRoutes)
+
+@accountRoutes.route("/add_account", methods = ["GET"])
+def new_account():
+    return "New Account Route"
---
+++ backend/app/config.py
@@ -0,0 +1,6 @@
+import os
+
+MYSQL_PASS = os.environ['MYSQL_408_PASS']
+MYSQL_HOST = os.environ['MYSQL_408_HOST']
+MYSQL_DB = os.environ['MYSQL_408_DB']
+MYSQL_ROOT = os.environ['MYSQL_408_ROOT']
