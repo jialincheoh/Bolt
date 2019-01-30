3e1c7b6fb43057eeaeecc5898c394974fbd658e7
jialincheoh <jcheoh@purdue.edu>
2019-01-30 11:17:18 -0500

    Auth0 authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './Form'
-
-
-// This import loads the firebase namespace.
-import firebase from 'firebase/app';
- 
-// These imports load individual services into the firebase namespace.
-import 'firebase/auth';
-// import 'firebase/database';
-
-const app = firebase.initializeApp({
-	apiKey: "AIzaSyBusHXXdt9QtbtFfSE9zlSV5Hjwrx6rPRo",
-    authDomain: "bolt-33b49.firebaseapp.com",
-    databaseURL: "https://bolt-33b49.firebaseio.com",
-    projectId: "bolt-33b49",
-    storageBucket: "bolt-33b49.appspot.com",
-    messagingSenderId: "578966418932"
-})
-
-
-
+import Auth from "./Auth"
+import Util from "./Util"
 
 class App extends Component {
 
@@ -29,16 +11,15 @@ class App extends Component {
 	}
 
 	componentDidMount(){
-		firebase.auth().onAuthStateChanged((user) => {
-			this.setState({user})
-		  if (user) {
-		    // User is signed in.
-		    console.log("USER:", user)
-		  } else {
-		    // No user is signed in.
-		    console.log('User is NOT signed in.')
-		  }
-		});
+		Auth.checkAuthentication((err, authResult) => {
+			if (err) { return Util.error(err); }
+			if (authResult) {
+				Auth.getProfile((err, user) => {
+					if (err) { return Util.error(err); }
+					this.setState({ user })	
+				});
+			}
+		})
 	}
 
   render() {
--- src/Form.js
+++ src/Form.js
@@ -1,76 +1,30 @@
 import React from 'react'
 import './Form.css'
 import bootstrapLogoImg from './images/bootstrap-solid.svg'
-import firebase from 'firebase/app';
-
+import Auth from './Auth';
 
 class Form extends React.Component {
-
-  signOut = () => {
-    firebase.auth().signOut();
-  }
-
-
-  googleSignIn = () => {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-  }
-
-  render(){
+  render () {
     const { user } = this.props;
-
-
     return (
-       <form className="form-signin">
-
-
-
-
+       <div className="form-signin">
         <img className="mb-4" src={bootstrapLogoImg} alt="" width="72" height="72" />
-        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label htmlFor="inputEmail" className="sr-only">Email address</label>
-        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus/>
-        <label htmlFor="inputPassword" className="sr-only">Password</label>
-        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me"/> Remember me
-          </label>
-        </div>
-        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <h1 className="h3 mb-3 font-weight-normal">{ user ? "Welcome!" : "Please sign in" }</h1>
         
-
         <div className="loginContainer"> 
         { user ?
           (<div>
-            <p>You are logged in as {user.displayName || user.email}</p>
-            <button className="btn btn-lg btn-primary btn-block" onClick={this.signOut}>Log out</button>
+            <p>You are logged in as {user.name || user.email}</p>
+            <button className="btn btn-lg btn-primary btn-block" onClick={e => Auth.logout()}>Log out</button>
             </div>)
           :
-          <button className="btn btn-lg btn-primary btn-block" onClick={this.googleSignIn}>Login with Google</button>
+          <button className="btn btn-lg btn-primary btn-block" onClick={e => Auth.login()}>Login with Auth0</button>
         }
 
         </div>
-
-
         <p className="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-      </form>
-      )
+      </div>
+    )
   }
 }
 
